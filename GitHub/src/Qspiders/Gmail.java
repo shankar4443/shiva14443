abd063265534ecd7d5666d701b516e93337a0f81
shank <shank@SHANKAR>
2019-02-25 16:25:19 +0530

    hi this is my second github demo file

---
+++ GitHub/src/Qspiders/Gmail.java
@@ -0,0 +1,12 @@
+package Qspiders;
+
+public class Gmail {
+	public void test() {
+		System.out.println("hi hello this is my first github demo file");
+	}
+	public static void main(String[] args) {
+		Gmail g=new Gmail();
+		g.test();
+	}
+
+}
